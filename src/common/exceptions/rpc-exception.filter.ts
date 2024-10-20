101ae7d6d6dc00b0857f99afe84a4b874c43b9d0
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-10-20 15:46:43 -0500

    Update rpc-exception.filter.ts

--- src/common/exceptions/rpc-exception.filter.ts
+++ src/common/exceptions/rpc-exception.filter.ts
@@ -1,35 +1,48 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus, RpcExceptionFilter } from "@nestjs/common";
-import { RpcException } from "@nestjs/microservices";
-import { Response } from "express";
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { Response } from 'express';
 
-export type MyRpcError ={
-    message: string;
-    status: HttpStatus | string;
-}
+export type MyRpcError = {
+  message: string;
+  status: HttpStatus | string;
+};
 
 @Catch(RpcException)
 export class MyRpcExceptionFilter implements ExceptionFilter {
-    catch(exception: RpcException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response: Response= ctx.getResponse();
-        const rpcError = exception.getError() as MyRpcError;
-        console.log("asokdasokaspok",rpcError)
+  catch(exception: RpcException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response: Response = ctx.getResponse();
+    const rpcError = exception.getError() as MyRpcError;
 
-        rpcError.status = rpcError.status == 'error'? HttpStatus.INTERNAL_SERVER_ERROR : rpcError.status
-
-        if (
-            typeof rpcError === 'object' &&
-            'status' in rpcError &&
-            'message' in rpcError
-          ) {
-            const status = isNaN(+rpcError.status) ? 400 :+rpcError.status;
-            return response.status(status).json(rpcError);
-          }
-      
-          response.status(400).json({
-            status: 400,
-            message: rpcError,
-          });
-
+    if(rpcError.toString().includes('Empty response')){
+      return response.status(500).json({
+        status: 500,
+        message: rpcError.toString().substring(0, rpcError.toString().indexOf('(') - 1);
+      })
     }
+
+    rpcError.status =
+      rpcError.status == 'error'
+        ? HttpStatus.INTERNAL_SERVER_ERROR
+        : rpcError.status;
+
+    if (
+      typeof rpcError === 'object' &&
+      'status' in rpcError &&
+      'message' in rpcError
+    ) {
+      const status = isNaN(+rpcError.status) ? 400 : +rpcError.status;
+      return response.status(status).json(rpcError);
+    }
+
+    response.status(400).json({
+      status: 400,
+      message: rpcError,
+    });
+  }
 }
